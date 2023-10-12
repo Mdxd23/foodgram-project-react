c766fb5e813c180c6507c616705900e6d09615fb
Mark Deikin <marickod@gmail.comgit config --global user.email marickod@gmail.com>
2023-10-12 21:13:21 +0300

    Добавил postgres, изменил dockerfile, добавил docker-compose

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -78,7 +78,18 @@ if DEBUG:
         }
     }
 else:
-    pass
+    DATABASES = {
+        'default': {
+            # Меняем настройку Django: теперь для работы будет использоваться
+            # бэкенд postgresql
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB', 'django'),
+            'USER': os.getenv('POSTGRES_USER', 'django'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+            'HOST': os.getenv('DB_HOST', ''),
+            'PORT': os.getenv('DB_PORT', 5432)
+        }
+    }
 
 
 # Password validation
