ce2ea065f76af6109cd8ee4ff0186463d3f1b55e
Mark Deikin <marickod@gmail.comgit config --global user.email marickod@gmail.com>
2023-10-15 15:55:37 +0300

    Добавил serializer base64image

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,10 @@
+import base64
+from django.core.files.base import ContentFile
 from rest_framework import serializers
 from djoser.serializers import UserSerializer, UserCreateSerializer
 from recipes.models import (Ingredient, Tag, Recipe,
                             IngredientInRecipe, Favorite)
 from users.models import User, Subscription
-from drf_extra_fields.fields import Base64ImageField
 
 
 class CustomUserSerializer(UserSerializer):
@@ -24,6 +25,17 @@ class CustomUserCreateSerializer(UserCreateSerializer):
     class Meta:
         model = User
         fields = ('__all__')
+
+
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+
+        return super().to_internal_value(data)
 
 
 class IngredientSerializer(serializers.ModelSerializer):
